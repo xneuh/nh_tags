95c2711163d33286dd1ec3716189b1183117a865
neuh <93486145+xneuh@users.noreply.github.com>
2023-03-13 15:20:20 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,13 @@ game 'gta5'
 lua54 'yes'
 
 client_scripts {
+	'@es_extended/imports.lua',
 	"settings.lua",
 	"client/cl_main.lua"
 }
 
 server_scripts {
+	'@es_extended/imports.lua',
 	"settings_sv.lua",
 	"@oxmysql/lib/MySQL.lua",
 	"server/sv_main.lua"
